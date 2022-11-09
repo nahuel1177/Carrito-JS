6ef2319b416d13f81051718ca870e37953558c15
Nahuel Enríquez <nahuelenriquez@gmail.com>
2022-11-09 03:17:17 -0300

    Segunda entrega,

--- script.js
+++ script.js
@@ -1,38 +1,114 @@
-let precioNotebook = 150000;
-let precioDesktop = 180000;
+const producto1 = {
 
-function PrecioNotebook(opcion){
-    if(opcion==1){
-        let precioFinal = precioNotebook * 1.21;
-        alert("El precio final es: " + precioFinal);
-    }else {
-        alert("El precio sin IVA incluido es: " + precioNotebook);
-    }
+    id: 0,
+    nombre: 'Desktop',
+    precio: 130,
+
 }
 
-function PrecioDesktop(opcion){
-    if(opcion==1){
-        let precioFinal = precioDesktop * 1.21;
-        alert("El precio final es: " + precioFinal);
-    }else {
-        alert("El precio sin IVA incluido es: " + precioDesktop);
-    }
+const producto2 = {
+
+    id: 1,
+    nombre: 'Laptop',
+    precio: 350,
+
 }
 
-function Menu(opcion) {
+const producto3 = {
+
+    id: 2,
+    nombre: 'Keyword',
+    precio: 120,
+
+}
+
+const producto4 = {
+
+    id: 3,
+    nombre: 'Mouse',
+    precio: 90,
+
+}
+
+function itemAgregado() {
+
+    alert('Producto agregado al carrito!');
+
+}
+
+function agregarCarrito(opcion) {
 
     switch (opcion) {
 
-        case '1':
-            let precio1 = prompt("¿Desea ver precio con IVA incluido?\n1 - SI\n2 - NO");
-            PrecioNotebook(precio1);
+        case '1': carrito.push(producto1.precio);
+            itemAgregado();
             break;
-        case '2':
-            let precio2 = prompt("¿Desea ver precio con IVA incluido?\n1 - SI\n2 - NO");
-            PrecioDesktop(precio2);
+        case '2': carrito.push(producto2.precio);
+            itemAgregado();
+            break;
+        case '3': carrito.push(producto3.precio);
+            itemAgregado();
+            break;
+        case '4': carrito.push(producto4.precio);
+            itemAgregado();
             break;
     }
 }
 
-let opcionMenu = prompt("Elegi equipo a comprar:\n\n" + "1 - Notebook\n2 - Desktop\n");
-Menu(opcionMenu);
+function sumarTotal() {
+
+    let total = carrito.reduce((a, b) => a + b, 0)
+
+    return total;
+}
+
+function Venta() {
+
+    if (carrito.length != 0) {
+        var monto = sumarTotal();
+        alert('El monto a pagar es: $' + monto + '\nGracias por su compra!');
+    }
+}
+
+function Cierre(){
+    if(venta == true){
+        Venta();
+        }else alert('No hay realizado compras. Gracias por visitar nuestro Sitio!');
+}
+
+function Menu() {
+    venta = true;
+    var opcion = prompt("Elige producto:\n\n" + "1 - Notebook\n2 - Desktop\n3 - Teclado\n4 - Mouse\n0 - Terminar Compra");
+
+    while (opcion != 0) {
+
+        if ((opcion < 0) || (opcion > 4)) {
+            alert('No tenemos ese producto');
+            Menu();
+        }
+
+        while ((opcion > 0) && (opcion < 5)) {
+
+            agregarCarrito(opcion);
+            opcion = prompt("Elige producto:\n\n" + "1 - Notebook\n2 - Desktop\n3 - Teclado\n4 - Mouse\n0 - Terminar Compra");
+
+        }
+    }
+    if (carrito.length == 0) {
+        let opcion2 = prompt('No hay productos en su carrito.\nEsta seguro que no desea comprar?\n1 - Seguir Comprando\n2 - Cerrar Carrito')
+        switch (opcion2) {
+            case '1': Menu();
+                break;
+            case '2': venta = false;
+                break;
+        }
+    }
+
+}
+
+var carrito = [];
+let venta;
+Menu();
+Cierre();
+
+
